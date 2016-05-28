4d2b2ed43f9dcca72a2e9f33808fed439cfbd6b3
Jonathan Craig <jonnydev@live.com>
2016-05-28 01:11:48 -0400

    Remove and Ignore Generated Files

--- app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var core_1 = require('@angular/core');
-core_1.enableProdMode();
-var app_component_1 = require('./app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=main.js.map
--- app/story-posts/story-posts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./story-post.service'));
-__export(require('./story-posts.component'));
-//# sourceMappingURL=story-posts.js.map
